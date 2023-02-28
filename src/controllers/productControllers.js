d4ce3d7918bbfccb294d12eaf2f6cd8362830197
Gabriel Bocchio <bocchio.gabriel@gmail.com>
2023-02-28 11:56:00 +0100

    commit de productdetail que quedaban sin cambiar

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -2,6 +2,9 @@ const productos={
     agregarProdu: (req,res)=> {
         return res.render("productDetail");
     },
+    carrito: (req,res)=> {
+        return res.render("productCart");
+    },
 
 }
 
