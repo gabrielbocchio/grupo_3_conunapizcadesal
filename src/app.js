421dc38b30dfe0f93a147b0e7f93940133fa6a3f
Gabriel Bocchio <bocchio.gabriel@gmail.com>
2023-03-03 14:24:04 +0100

    agregado comentarios y organizacion de rutas/controllers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-
-const express = require('express');
+// Requerimos express y lo ejecutamos para tener disponibles todos los metodos que vamos a precisar
+const express = require("express");
+const app = express();
 // Path : poder unificar las rutas dentros de los distintos sistemas operartivos
 const path = require ('path')
 
-const app = express();
-
-/* routes */
+// Importar los distintos enrutadores
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const mainRoutes = require("./routes/mainRoutes");
@@ -15,16 +14,31 @@ const carritoRoutes = require("./routes/carritoRoutes");
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
-/* levanto server */
-app.listen(3000,()=> console.log('Servidor corriendo en el puerto 3000'));
 
-// ************ Template Engine ************
+// -- Template Engine --
 app.set("view engine", "ejs"); // motor de vistas
-app.set("views", "../src/views"); // define la ubicacion de la carpeta vistas
+app.set('views', path.join(__dirname, 'views'));// define la ubicacion de la carpeta vistas
 
-/* llamado de rutas!! */
+// Usando los enrutadores importados
 app.use('/', mainRoutes);
 app.use('/productDetail', productRoutes);
 app.use('/login', userRoutes);
 app.use('/productCart', carritoRoutes);
 
+//estos prox 2 son para el metodo POST, para que pueda leer req.body (req.body en POST es el 'req.query' del get)
+app.use(express.urlencoded({ extended:false }));
+app.use(express.json());
+
+//pasar como metodo el paquete instalado para usar el metodo PUT (npm install method-override --save))
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"))
+
+// error 404 tiene q ir siempre al final, clave
+app.use((req,res, next)=> {
+    res.status(404).send("404, pagina no encontrada")
+}); 
+
+/* levantar el server */
+app.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000")
+});
--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,5 +1,6 @@
+// Creamos el objeto literal con los métodos a exportar
 const main ={
-
+// Manejo del pedido get con ruta
     index: (req,res)=> {
         return res.render("main/home");
     },
@@ -12,5 +13,5 @@ const main ={
 
 }
 
-
+// Exportamos el objeto literal con los distintos métodos, que se usará en el enrutador por defecto
 module.exports=main;
