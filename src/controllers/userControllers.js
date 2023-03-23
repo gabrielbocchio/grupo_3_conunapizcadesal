37860b2331a1acc1a6abd34994308ae4e4e2f28a
Gabriel Bocchio <bocchio.gabriel@gmail.com>
2023-03-23 11:54:39 +0100

    ahora pushea users nuevos a users.json

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,11 +1,57 @@
-const users={
+const fs = require("fs");
+const path = require("path");
+const usuariosFilePath = path.join(__dirname, "../database/users.json");
+const { validationResult } = require('express-validator');
 
+const users={
+/* -- renderizo el get del log-in -- */
     login: (req,res)=> {
         return res.render("users/login");
     },
+/* -- renderizo el get del sign-in -- */
      signin: (req,res)=> {
         return res.render("users/signin");
     }, 
+
+    processLogin: (req,res)=> {
+        return res.send("probando el post che");
+    },
+
+    /* creacion de usuario y pusheo a json, pasado con validaciones */
+    processSignin: (req,res)=> {
+        
+        const resultValidation = validationResult(req);
+        if (resultValidation.errors.length > 0) {
+            return res.render('users/signin', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }
+        const users = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8'));
+        
+        let usuario = {
+            nombre: req.body.firstname,
+            apellido: req.body.lastname,
+            id: users[users.length - 1].id + 1,
+            email: req.body.email,
+            password: req.body.password,
+            repeatpassword: req.body.repeatpassword,
+            imagen: req.file ? req.file.filename : "default-image.png"
+          };
+  
+        // Aplicar código para hacer lo que se necesita
+         users.push(usuario);
+        // Convertir el arreglo de productos a formato JSON
+        let usersJSON = JSON.stringify(users, null, " ");
+        // Escribir el archivo con los nuevos datos
+        fs.writeFileSync(usuariosFilePath, usersJSON);
+
+
+		return res.redirect("/");
+
+    }, 
+
+
 
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,65 @@
+// ************ Require's ************
 const express = require("express");
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
+const { body, check } = require('express-validator');
 
+// ************ Controller Require ************
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/images/avatars");
+    },
+    filename: function (req, file, cb) {
+    cb (null, file.fieldname + '-'+ Date.now() + path.extname(file.originalname))
+}
+
+});
+
+// ************************
+const upload = multer({ storage: storage });
+// ************************
 const userController = require("../controllers/userControllers");
+// ************************
 
+const validations = [
+	body('firstname').notEmpty().withMessage('Tienes que escribir tu nombre'),
+	body('lastname').notEmpty().withMessage('Tienes que escribir tu apellido'),
+ 	body('email')
+		.notEmpty().withMessage('Tienes que escribir un correo electrónico').bail()
+		.isEmail().withMessage('Debes escribir un formato de correo válido'),
+	body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+    body('repeatpassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+        throw new Error('Las contraseñas no coinciden');
+            }
+         return true;
+        }),
+	body('avatars').custom((value, { req }) => {
+		let file = req.file;
+		let acceptedExtensions = ['.jpg', '.png', '.gif', 'jpeg'];
+		
+		if (!file) {
+			throw new Error('Tienes que subir una imagen');
+		} else {
+			let fileExtension = path.extname(file.originalname);
+			if (!acceptedExtensions.includes(fileExtension)) {
+				throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+			}
+		}
+
+		return true;
+	})
+] 
+
+// ************************
+// ************************
 router.get('/login', userController.login);
+router.post('/login', userController.processLogin);
+
 router.get('/signin', userController.signin);
+router.post('/signin', upload.single("avatars"), validations,userController.processSignin);
+
+
 
 module.exports=router;
